11319a841ffb84255f3306004c977e8e656b6b3f
Noah Gift <noah.gift@gmail.com>
2021-04-14 17:47:55 +0000

    adding more

--- app.py
+++ app.py
@@ -3,10 +3,15 @@
 
 
 def add(x, y):
-
+    #import pdb;pdb.set_trace()
     result = x + y
     print(f"This is the x: {x}, y: {y} and this is the result: {result}")
     return result
 
+def double():
+    xx = add(1,1)
+    yy = add(2,2)
+    return xx+yy
 
 print(add(1, 1))
+print(double())
